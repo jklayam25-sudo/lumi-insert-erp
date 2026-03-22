1ab9b68e76647a0c2277a8a2e9ec078805d01580
jekael <224990958+jklayam25-sudo@users.noreply.github.com>
2026-03-23 01:56:33 +0700

    security(role): implement role hierarchy

---
+++ src/main/java/lumi/insert/app/config/security/RoleHierarchyConfig.java
@@ -0,0 +1,28 @@
+package lumi.insert.app.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
+import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+
+@Configuration
+public class RoleHierarchyConfig {
+    
+    @Bean
+    static RoleHierarchy roleHierarchy(){
+        return RoleHierarchyImpl.fromHierarchy("""
+                ROLE_OWNER > ROLE_FINANCE
+                ROLE_OWNER > ROLE_WAREHOUSE
+                ROLE_OWNER > ROLE_CASHIER 
+                """);
+    }
+
+    @Bean
+    static MethodSecurityExpressionHandler methodSecurityExpressionHandler(RoleHierarchy roleHierarchy) {
+        DefaultMethodSecurityExpressionHandler expressionHandler = new DefaultMethodSecurityExpressionHandler();
+        expressionHandler.setRoleHierarchy(roleHierarchy);
+        return expressionHandler;
+    }
+}
