beaa6367c8fd9bdc0cc476b574f30a66e2d532f1
jekael <224990958+jklayam25-sudo@users.noreply.github.com>
2026-03-23 07:27:15 +0700

    feat(mail-sender): implement mail sender producer to message broker

--- src/main/java/lumi/insert/app/service/implement/MessageProducerServiceImpl.java
+++ src/main/java/lumi/insert/app/service/implement/MessageProducerServiceImpl.java
@@ -1,11 +1,12 @@
 package lumi.insert.app.service.implement;
-
+  
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import lombok.extern.slf4j.Slf4j;
 import lumi.insert.app.activitycore.entity.ActivityLog;
+import lumi.insert.app.core.entity.nondatabase.TransactionInvoiceMail;
 import lumi.insert.app.service.MessageProducerService;
 
 @Service
@@ -19,5 +20,12 @@ public class MessageProducerServiceImpl implements MessageProducerService{
     public void sendActivityLog(ActivityLog activityLog) { 
         rabbitTemplate.convertAndSend("main-exchange", "activity-routing", activityLog);
     }
+
+    @Override
+    public void sendTransactionInvoiceEmail(TransactionInvoiceMail request) {
+         rabbitTemplate.convertAndSend("main-exchange", "transaction-invoice-routing", request);
+    }
+
+    
     
 }
