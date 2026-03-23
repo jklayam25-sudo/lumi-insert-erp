77b7e38bea2f78b2be81bae436d031799ab7d7fc
jekael <224990958+jklayam25-sudo@users.noreply.github.com>
2026-03-23 07:27:46 +0700

    feat(mail-sender): implement mail sender consumer

--- src/main/java/lumi/insert/app/worker/MessageConsumer.java
+++ src/main/java/lumi/insert/app/worker/MessageConsumer.java
@@ -1,12 +1,17 @@
-package lumi.insert.app.worker;
+package lumi.insert.app.worker; 
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
+import jakarta.mail.MessagingException;
 import lombok.extern.slf4j.Slf4j;
 import lumi.insert.app.activitycore.entity.ActivityLog;
-import lumi.insert.app.activitycore.repository.ActivityLogRepository;
+import lumi.insert.app.activitycore.repository.ActivityLogRepository; 
+import lumi.insert.app.core.entity.nondatabase.TransactionInvoiceMail;
+import lumi.insert.app.service.MailSenderService;
 
 @Component
 @Slf4j
@@ -14,9 +19,25 @@ public class MessageConsumer {
 
     @Autowired
     ActivityLogRepository activityLogRepository;
-    
+
+    @Autowired
+    MailSenderService mailSenderService; 
+
     @RabbitListener(queues = "activity-logs")
     void activityLogsHandler(ActivityLog activityLog){ 
         activityLogRepository.save(activityLog);
     }
+
+    @RabbitListener(queues = "transaction-invoice-mail")
+    void transactionInvoiceMailHandler(TransactionInvoiceMail request) throws MessagingException{ 
+        try {
+            SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken(request.auth(), null, null));
+            mailSenderService.sendTransactionInvoice(request);    
+        } catch (Exception e) {
+            log.error("Failed to send, with message: " + e.getMessage());
+            throw e;
+        }
+                
+    }
+
 }
