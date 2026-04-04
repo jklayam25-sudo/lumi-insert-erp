798776be306f898c60c685dc340a3e1779a44e3a
jekael <224990958+jklayam25-sudo@users.noreply.github.com>
2026-04-04 18:53:40 +0700

    feat: add methods for uploading customer and employee pictures, and handle file storage events

--- src/main/java/lumi/insert/app/service/implement/CustomerServiceImpl.java
+++ src/main/java/lumi/insert/app/service/implement/CustomerServiceImpl.java
@@ -1,5 +1,9 @@
 package lumi.insert.app.service.implement;
- 
+  
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID; 
 
 import org.springframework.beans.factory.annotation.Autowired; 
@@ -9,14 +13,19 @@ import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.github.f4b6a3.uuid.UuidCreator;
 
 import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
 import lumi.insert.app.aspect.annotation.ActivityLogger;
 import lumi.insert.app.core.entity.Customer;
+import lumi.insert.app.core.entity.CustomerPicture; 
 import lumi.insert.app.core.entity.nondatabase.ActivityAction;
+import lumi.insert.app.core.entity.nondatabase.CloudinaryResponse;
 import lumi.insert.app.core.entity.nondatabase.SliceIndex;
+import lumi.insert.app.core.repository.CustomerPictureRepository;
 import lumi.insert.app.core.repository.CustomerRepository;
 import lumi.insert.app.dto.request.CustomerCreateRequest;
 import lumi.insert.app.dto.request.CustomerGetByFilter;
@@ -25,14 +34,18 @@ import lumi.insert.app.dto.request.CustomerUpdateRequest;
 import lumi.insert.app.dto.response.CustomerDetailResponse;
 import lumi.insert.app.dto.response.CustomerNameResponse;
 import lumi.insert.app.dto.response.CustomerResponse;
+import lumi.insert.app.exception.DatabaseInternalException;
 import lumi.insert.app.exception.DuplicateEntityException;
 import lumi.insert.app.exception.NotFoundEntityException;
+import lumi.insert.app.exception.StorageActionException;
 import lumi.insert.app.mapper.CustomerMapper;
 import lumi.insert.app.service.CustomerService;
+import lumi.insert.app.service.StorageService;
 import lumi.insert.app.utils.generator.JpaSpecGenerator;
 
 @Service
 @Transactional
+@Slf4j
 public class CustomerServiceImpl implements CustomerService{
 
     @Autowired
@@ -43,6 +56,12 @@ public class CustomerServiceImpl implements CustomerService{
 
     @Autowired
     JpaSpecGenerator jpaSpec;
+
+    @Autowired
+    StorageService storageService;
+
+    @Autowired
+    CustomerPictureRepository customerPictureRepository;
 
     @Override
     @ActivityLogger(
@@ -107,6 +126,54 @@ public class CustomerServiceImpl implements CustomerService{
 
         customerMapper.updateEntityFromDto(request, customer); 
         return customerMapper.createDtoDetailResponseFromEmployee(customer);
+    }
+
+    @Override
+    public Boolean addCustomerPicture(UUID id, MultipartFile[] files) {
+
+        Customer customer = customerRepository.findById(id)
+            .orElseThrow(() -> new NotFoundEntityException("Customer with id " + id + " is not found"));
+            
+        List<String> pictureUrl = customer.getPictureUrl();
+        if(pictureUrl == null) pictureUrl = new ArrayList<>();
+
+        log.info("Customer: {}", customer);
+        String fileName = customer.getName() + "-";
+        List<CustomerPicture> customerPictures = new ArrayList<>();
+        List<String> picturesId = new ArrayList<>();
+        try {
+            for (MultipartFile file : files) { 
+                CloudinaryResponse upload = storageService.uploadImageSync(file.getBytes(), fileName + LocalDateTime.now() , "customer");
+
+                CustomerPicture customerPicture = CustomerPicture.builder()
+                .id(UuidCreator.getTimeOrderedEpochFast())
+                .pictureUrl(upload.getSecureUrl())
+                .customer(customer)
+                .publicId(upload.getPublicId())
+                .build(); 
+
+                customerPictures.add(customerPicture);
+                picturesId.add(upload.getPublicId());
+                pictureUrl.add(upload.getSecureUrl()); 
+            }
+            customer.setPictureUrl(pictureUrl);
+            customerPictureRepository.saveAll(customerPictures);
+        } catch (IOException e) {
+            e.printStackTrace();
+            log.error("Upload failed, messages: " + e.getMessage());
+            throw new StorageActionException("Server couldn't complete the request due to internal problem, try again or contact developer");
+        } catch (Exception e) { 
+            log.error("Save to database failed, attempting to delete image at storage. Messages: {}", e.getMessage()); 
+
+            picturesId.forEach(pictureId -> {
+                 if(!(storageService.deleteImage(pictureId))) log.error("Failed to delete image with publicId: {}", id);
+            }); 
+
+            throw new DatabaseInternalException("Server couldn't complete the request due to internal problem, try again or contact developer");
+        }
+        
+        return true;
+
     }
     
 }
--- src/main/java/lumi/insert/app/service/implement/EmployeeServiceImpl.java
+++ src/main/java/lumi/insert/app/service/implement/EmployeeServiceImpl.java
@@ -1,34 +1,44 @@
 package lumi.insert.app.service.implement;
- 
-import java.util.UUID;
+   
+import java.io.IOException; 
+import java.util.UUID; 
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired; 
 import org.springframework.data.domain.Pageable; 
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.Sort;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-
+import org.springframework.web.multipart.MultipartFile;
+ 
 import com.github.f4b6a3.uuid.UuidCreator;
 
 import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
 import lumi.insert.app.aspect.annotation.ActivityLogger;
 import lumi.insert.app.core.entity.Employee;
+import lumi.insert.app.core.entity.EmployeePicture;
 import lumi.insert.app.core.entity.nondatabase.ActivityAction;
+import lumi.insert.app.core.entity.nondatabase.CloudinaryResponse;
 import lumi.insert.app.core.repository.AuthTokenRepository;
+import lumi.insert.app.core.repository.EmployeePictureRepository;
 import lumi.insert.app.core.repository.EmployeeRepository;
 import lumi.insert.app.dto.request.EmployeeCreateRequest;
 import lumi.insert.app.dto.request.EmployeeUpdateRequest;
 import lumi.insert.app.dto.request.PaginationRequest;
 import lumi.insert.app.dto.response.EmployeeResponse;
-import lumi.insert.app.exception.DuplicateEntityException;
+import lumi.insert.app.exception.DatabaseInternalException;
+import lumi.insert.app.exception.DuplicateEntityException; 
 import lumi.insert.app.exception.NotFoundEntityException;
+import lumi.insert.app.exception.StorageActionException;
 import lumi.insert.app.mapper.EmployeeMapper;
 import lumi.insert.app.service.EmployeeService;
+import lumi.insert.app.service.StorageService;
 
 @Service
 @Transactional
+@Slf4j
 public class EmployeeServiceImpl implements EmployeeService{
 
     @Autowired
@@ -41,7 +51,13 @@ public class EmployeeServiceImpl implements EmployeeService{
     BCryptPasswordEncoder passwordEncoder;
 
     @Autowired
-    EmployeeMapper employeeMapper;
+    EmployeeMapper employeeMapper; 
+
+    @Autowired
+    StorageService storageService;
+
+    @Autowired
+    EmployeePictureRepository employeePictureRepository;
 
     @Override
     @ActivityLogger(
@@ -126,6 +142,46 @@ public class EmployeeServiceImpl implements EmployeeService{
         if(!employee.isActive()) authTokenRepository.deleteByEmployeeId(employee.getId());
 
         return employeeMapper.createDtoResponseFromEmployee(employee);
+    }
+
+    @Override
+    public boolean setEmployeeProfile(UUID id, MultipartFile file){ 
+
+        Employee employee = employeeRepository.findById(id)
+            .orElseThrow(() -> new NotFoundEntityException("Employee with ID " + id + " was not found"));
+        
+        String fileName = employee.getUsername() + "-profile";
+
+        String publicId = null;
+
+        try { 
+            CloudinaryResponse upload = storageService.uploadImageSync(file.getBytes(), fileName ,"employee");
+
+            EmployeePicture employeePicture = EmployeePicture.builder()
+            .id(UuidCreator.getTimeOrderedEpochFast())
+            .pictureUrl(upload.getSecureUrl())
+            .employee(employee)
+            .publicId(upload.getPublicId())
+            .build();  
+
+            publicId = upload.getPublicId();
+
+            employeePictureRepository.save(employeePicture);
+            employee.setPictureUrl(String.valueOf(upload.getSecureUrl()));
+            return true; 
+        } catch (IOException e) { 
+            log.error("Upload failed, messages: {}", e.getMessage());
+            throw new StorageActionException("Server couldn't complete the request due to internal problem, try again or contact developer");
+        } catch (Exception e) { 
+            log.error("Save to database failed, attempting to delete image at storage. Messages: {}", e.getMessage()); 
+
+            if(publicId != null) {
+                if(!(storageService.deleteImage(publicId))) log.error("Failed to delete image with publicId: {}", publicId);
+            }
+
+            throw new DatabaseInternalException("Server couldn't complete the request due to internal problem, try again or contact developer");
+        } 
+  
     }
     
 }
