67a95b40ac3e5162adb7a77af46cabf00abeaa6c
Jekael <224990958+jklayam25-sudo@users.noreply.github.com>
2026-04-11 17:11:46 +0700

    fix: fix CORS rules colliding with nginx

--- src/main/java/lumi/insert/app/config/security/SecurityConfig.java
+++ src/main/java/lumi/insert/app/config/security/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .cors(cors -> cors.configure(http))
+            .cors(cors -> cors.disable())
             .csrf(csrf -> csrf.disable()) 
             .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
             .exceptionHandling(exc -> exc.accessDeniedHandler(customAccessDeniedHandler()));
